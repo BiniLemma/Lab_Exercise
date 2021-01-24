56cd7f48d16fce329fd390fb65a9adf4434518ce
BiniLemma <it.binyam.lemma@gmail.com>
2021-01-24 23:39:05 +0300

    mbi

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ var lastName;
 var age;
 var job;
 // Receive the values from input
-firstName = prompt("Enter Your First Name");
-lastName = prompt("Enter Your Last Name");
-job = prompt("What is Your Profession ?")
-age = prompt("Enter Your Age");
+firstName = prompt("Enter Your First Name: ");
+lastName = prompt("Enter Your Last Name: ");
+job = prompt("What is Your Profession? ")
+age = prompt("Enter Your Age: ");
 // Display the result on console from input
 console.log("Here is your Profile ")
 console.log("Full Name: " + firstName + " " + lastName);
